541b1e4c573a0625bb3e55ee499773ff3b22c3b7
Sparthan419 <barriosjoan@yahoo.com>
2022-05-13 15:46:57 -0500

    conexion db
    conexion db

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 
 const TaskRouter = require('./routes/task');
 
+const config = require('./config');
+const database = require('./services/database');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/task', TaskRouter);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("nodejs api running...");
+database.connect(config.database);
+
+app.listen(config.port, () => {
+    console.log(`nodejs api running at ${config.port}...`);
 });
